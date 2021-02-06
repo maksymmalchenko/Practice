ce344fa4ae21d56adffe94395f4a4c8615855416
Maksym Malchenko <maksymmalchenkooff@gmail.com>
2021-02-07 01:18:22 +0200

    added module3 tasks

---
+++ module3/6.js
@@ -0,0 +1,25 @@
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const calculateTotalPrice = function(allProducts, productName) {
+  let totalProductPrice = 0;
+  
+  for (const product of allProducts) {
+    if (productName == product.name) {
+      totalProductPrice = product['price'] * product['quantity'];
+      
+      return totalProductPrice;
+    }
+  }
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(calculateTotalPrice(products, 'Радар')); // 5200
+
+console.log(calculateTotalPrice(products, 'Дроид')); // 2800

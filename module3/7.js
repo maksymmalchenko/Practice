7edc36662750fad284fc49e91059436e37defa8c
Maksym Malchenko <maksymmalchenkooff@gmail.com>
2021-02-08 12:07:14 +0200

    added smthing

--- module3/7.js
+++ module3/7.js
@@ -1,10 +1,16 @@
 /*
+Напиши скрипт управления личным кабинетом интернет банка. Есть 
+объект account в котором необходимо реализовать методы 
+для работы с балансом и историей транзакций.
+
+
+
  * Типов транзацкий всего два.
  * Можно положить либо снять деньги со счета.
  */
 const Transaction = {
-  DEPOSIT: 'deposit',
-  WITHDRAW: 'withdraw',
+    DEPOSIT: 'deposit',
+    WITHDRAW: 'withdraw',
 };
 
 /*
@@ -12,50 +18,90 @@ const Transaction = {
  */
 
 const account = {
-  // Текущий баланс счета
-  balance: 0,
+    // Текущий баланс счета
+    balance: 0,
 
-  // История транзакций
-  transactions: [],
+    // История транзакций
+    transactions: [],
 
-  /*
-   * Метод создает и возвращает объект транзакции.
-   * Принимает сумму и тип транзакции.
-   */
-  createTransaction(amount, type) {},
+    /*
+     * Метод создает и возвращает объект транзакции.
+     * Принимает сумму и тип транзакции.
+     */
+    createTransaction(amount, type) {
+        const transaction = {};
 
-  /*
-   * Метод отвечающий за добавление суммы к балансу.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций
-   */
-  deposit(amount) {},
+        transaction.id = Date.now();
+        // transaction.id = 228;
+        transaction.type = type;
+        transaction.amount = amount;
 
-  /*
-   * Метод отвечающий за снятие суммы с баланса.
-   * Принимает сумму танзакции.
-   * Вызывает createTransaction для создания объекта транзакции
-   * после чего добавляет его в историю транзакций.
-   *
-   * Если amount больше чем текущий баланс, выводи сообщение
-   * о том, что снятие такой суммы не возможно, недостаточно средств.
-   */
-  withdraw(amount) {},
+        return transaction;
+    },
 
-  /*
-   * Метод возвращает текущий баланс
-   */
-  getBalance() {},
+    /*
+     * Метод отвечающий за добавление суммы к балансу.
+     * Принимает сумму танзакции.
+     * Вызывает createTransaction для создания объекта транзакции
+     * после чего добавляет его в историю транзакций
+     */
+    deposit(amount) {
+        this.transactions.push(this.createTransaction(amount, Transaction.DEPOSIT));
+        this.balance += amount;
+    },
 
-  /*
-   * Метод ищет и возвращает объект транзации по id
-   */
-  getTransactionDetails(id) {},
+    /*
+     * Метод отвечающий за снятие суммы с баланса.
+     * Принимает сумму танзакции.
+     * Вызывает createTransaction для создания объекта транзакции
+     * после чего добавляет его в историю транзакций.
+     *
+     * Если amount больше чем текущий баланс, выводи сообщение
+     * о том, что снятие такой суммы не возможно, недостаточно средств.
+     */
+    withdraw(amount) {
+        if (amount > this.balance) {
+            console.log('Снятие такой суммы не возможно, недостаточно средств!');
+            return;
+        }
 
-  /*
-   * Метод возвращает количество средств
-   * определенного типа транзакции из всей истории транзакций
-   */
-  getTransactionTotal(type) {},
+        this.transactions.push(this.createTransaction(amount, Transaction.WITHDRAW));
+        this.balance -= amount;
+    },
+
+    /*
+     * Метод возвращает текущий баланс
+     */
+    getBalance() {
+        return this.balance;
+    },
+
+    /*
+     * Метод ищет и возвращает объект транзации по id
+     */
+    getTransactionDetails(id) {
+        for (const transaction of this.transactions) {
+            if (transaction.id === id) {
+                return transaction;
+            }
+        }
+
+        // console.log('Транзакция по ID не была найдена. Такой транзакции не существует!')
+    },
+
+    /*
+     * Метод возвращает количество средств
+     * определенного типа транзакции из всей истории транзакций
+     */
+    getTransactionTotal(type) {
+        let totalAmount = 0;
+
+        for (const transaction of this.transactions) {
+            if ((type = transaction.type)) {
+                totalAmount += transaction.amount;
+            }
+        }
+
+        return totalAmount;
+    },
 };
---
+++ module4/3.js
@@ -0,0 +1,44 @@
+const findBestEmployee = function(employees) {
+  const keys = Object.keys(employees);
+  
+  let bestResult = employees[keys[0]];
+  let bestEmployee = keys[0];
+  
+  for (const key of keys) {
+    if (bestResult < employees[key]) {
+      bestResult = employees[key];
+      bestEmployee = key;
+    }
+  }
+  
+  return bestEmployee;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
